33b5f1259d55cfc41801571f4543bb1e0e291242
Bastos974 <sebastien.lemoine974@gmail.com>
2023-03-28 20:55:47 +0400

    🍒App.js>compteur

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
+
 function App(){
 
 // State Etat Donnée
-
+const [number,setnumber]=useState(0)
 
 // Comporetement
-
+const handleClick = () => {
+    setnumber(number+1)
+}
+const handleClick2 = () => {
+    setnumber(number-1)
+}
 
 // Affichage (rerender)
 return <div>
-    <h1>salut seb</h1>
-    <h2>coucou</h2>
+    <h1>compteur</h1>
+    <button onClick={handleClick}> + </button>
+    <h2>{number}</h2>
+    <button onClick={handleClick2}> - </button>
+    
 </div>
 
 
