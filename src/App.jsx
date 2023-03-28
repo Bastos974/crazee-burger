cb06732e328ca1839d2b8e8b4fc44dded15c9842
Bastos974 <sebastien.lemoine974@gmail.com>
2023-03-28 20:09:55 +0400

    testtest

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App(){
+
+// State Etat Donnée
+
+
+// Comporetement
+
+
+// Affichage (rerender)
+return <div>
+    <h1>salut seb</h1>
+    <h2>coucou</h2>
+</div>
+
+
+
+}
+export default App;
+
